806e7012f9dd98a0ff5373e224e49a9f2887cbbe
Ovv <contact@ovv.wtf>
2017-12-28 13:51:26 +0100

    Force git clone

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,6 @@ pip install ansible
 openssl aes-256-cbc -K $encrypted_113807954c8a_key -iv $encrypted_113807954c8a_iv -in sirbot_travis_rsa.enc -out id_rsa -d
 chmod 600 id_rsa
 
-ansible-galaxy install -r requirements.yml
-
 echo -n "$ANSIBLE_PASSWORD" > .pass
 
 ansible-playbook playbook.yml --private-key=id_rsa --tags="deploy"
