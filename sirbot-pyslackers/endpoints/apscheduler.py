d6fccd810cefb9b0dfe468c8776a9223d155a883
Ovv <contact@ovv.wtf>
2018-02-07 21:10:48 +0100

    Exclude members from channel list
    Fix #48

--- sirbot-pyslackers/endpoints/apscheduler.py
+++ sirbot-pyslackers/endpoints/apscheduler.py
@@ -19,7 +19,11 @@ def create_jobs(scheduler, bot):
 async def slack_channel_list(bot):
     LOG.info('Updating list of slack channels...')
     async with bot['plugins']['pg'].connection() as pg_con:
-        async for channel in bot['plugins']['slack'].api.iter(methods.CHANNELS_LIST, minimum_time=3):
+        async for channel in bot['plugins']['slack'].api.iter(
+                methods.CHANNELS_LIST,
+                minimum_time=3,
+                data={'exclude_members': True}
+        ):
             await pg_con.execute('''INSERT INTO slack.channels (id, raw) VALUES ($1, $2)
                                     ON CONFLICT (id) DO UPDATE SET raw = $2''', channel['id'], channel)
     LOG.info('List of slack channels up to date.')
