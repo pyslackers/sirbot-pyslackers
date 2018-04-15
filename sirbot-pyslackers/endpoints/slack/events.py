2d8bfd0cb7e9e45fd828d0d71cd853234e132939
Ovv <contact@ovv.wtf>
2018-04-15 20:48:57 +0200

    Remove recording feature

--- sirbot-pyslackers/endpoints/slack/events.py
+++ sirbot-pyslackers/endpoints/slack/events.py
@@ -12,8 +12,6 @@ LOG = logging.getLogger(__name__)
 
 def create_endpoints(plugin):
     plugin.on_event('team_join', team_join, wait=False)
-    plugin.on_event('reaction_added', start_recording)
-    plugin.on_event('reaction_added', stop_recording)
     plugin.on_event('pin_added', pin_added)
 
 
@@ -34,65 +32,6 @@ async def team_join(event, app):
     message['user'] = event['user']['id']
 
     await app.plugins['slack'].api.query(url=methods.CHAT_POST_EPHEMERAL, data=message)
-
-
-async def start_recording(event, app):
-
-    if event['reaction'] == 'start_recording' and 'item' in event and event['item']['type'] == 'message':
-
-        async with app['plugins']['pg'].connection() as pg_con:
-            recording_id = await pg_con.fetchval(
-                '''INSERT INTO slack.recordings (start, "user", channel) VALUES ($1, $2, $3) RETURNING id''',
-                event['item']['ts'], event['user'], event['item']['channel']
-            )
-
-        message = Message()
-        message['text'] = 'Would you like to record this conversation ?'
-        message['attachments'] = [
-            {
-                'fallback': 'start recording',
-                'callback_id': 'recording',
-                'actions': [
-                    {
-                        'name': 'message',
-                        'text': 'Yes, Until this message',
-                        'style': 'primary',
-                        'type': 'button',
-                        'value': recording_id
-                    },
-                    {
-                        'name': 'emoji',
-                        'text': 'Yes, Until the :stop_recording: emoji',
-                        'style': 'primary',
-                        'type': 'button',
-                        'value': recording_id
-                    },
-                    {
-                        'name': 'cancel',
-                        'text': 'Cancel',
-                        'style': 'danger',
-                        'type': 'button',
-                        'value': recording_id
-                    }
-                ]
-            },
-        ]
-
-        message['channel'] = event['item']['channel']
-        message['user'] = event['user']
-
-        await app.plugins['slack'].api.query(url=methods.CHAT_POST_EPHEMERAL, data=message)
-
-
-async def stop_recording(event, app):
-
-    if event['reaction'] == 'stop_recording' and 'item' in event and event['item']['type'] == 'message':
-        async with app['plugins']['pg'].connection() as pg_con:
-            await pg_con.execute(
-                '''UPDATE slack.recordings SET "end" = $1 WHERE id = (SELECT id FROM slack.recordings WHERE "user" = $2
-                AND channel = $3 AND "end" is NULL ORDER BY created ASC LIMIT 1)''',
-                event['item']['ts'], event['user'], event['item']['channel']
-            )
 
 
 async def pin_added(event, app):
