fa211e8e4453a5ef0a4b9ce78d2a31ca76da9e91
Ovv <contact@ovv.wtf>
2019-02-19 16:23:19 +0100

    Sleep after docker push deployment

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,3 +2,6 @@
 echo "Uploading image to dockerhub"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push pyslackers/sirbot-pyslackers
+echo "Sleeping 120 seconds for new image to be handled by dockerhub"
+sleep 120
+
