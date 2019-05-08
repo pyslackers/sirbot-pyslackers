1388635679821824d5599900b1c9e8f41db5dece
Quentin Dawans <contact@ovv.wtf>
2019-05-08 19:10:45 +0200

    platformsh migration bis (#238)

--- sirbot-pyslackers/__main__.py
+++ sirbot-pyslackers/__main__.py
@@ -1,10 +1,12 @@
 import os
+import sys
+import asyncio
 import logging.config
 
 import yaml
 import raven
+import platformshconfig
 from sirbot import SirBot
-from raven.conf import setup_logging
 from raven.processors import SanitizePasswordsProcessor
 from sirbot.plugins.slack import SlackPlugin
 from sirbot.plugins.github import GithubPlugin
@@ -14,39 +16,78 @@ from sirbot.plugins.apscheduler import APSchedulerPlugin
 from sirbot.plugins.readthedocs import RTDPlugin
 
 from . import endpoints
-from .plugins import PypiPlugin, GiphyPlugin, DeployPlugin, StocksPlugin
+from .plugins import PypiPlugin, GiphyPlugin, StocksPlugin
 
 PORT = os.environ.get("SIRBOT_PORT", os.environ.get("PORT", 9000))
 HOST = os.environ.get("SIRBOT_ADDR", "127.0.0.1")
 VERSION = "0.0.11"
 LOG = logging.getLogger(__name__)
+PSH_CONFIG = platformshconfig.Config()
 
 
-def make_sentry_logger():
+def make_sentry_logger(dsn):
+
+    if PSH_CONFIG.is_valid_platform():
+        version = PSH_CONFIG.treeID
+    else:
+        version = VERSION
+
     client = raven.Client(
-        dsn=os.environ["SENTRY_DSN"],
-        release=os.environ["SIRBOT_VERSION"],
-        processor=SanitizePasswordsProcessor,
+        dsn=dsn, release=version, processor=SanitizePasswordsProcessor
     )
     handler = SentryHandler(client)
     handler.setLevel(logging.WARNING)
-    setup_logging(handler)
+    raven.conf.setup_logging(handler)
 
 
-if __name__ == "__main__":
-
+def setup_logging():
     try:
         with open(
             os.path.join(os.path.dirname(os.path.realpath(__file__)), "../logging.yml")
         ) as log_configfile:
-            logging.config.dictConfig(yaml.load(log_configfile.read()))
+            logging.config.dictConfig(yaml.safe_load(log_configfile.read()))
 
     except Exception as e:
         logging.basicConfig(level=logging.DEBUG)
         LOG.exception(e)
 
-    if "SENTRY_DSN" in os.environ:
-        make_sentry_logger()
+    sentry_dsn = os.environ.get("SENTRY_DSN")
+    if sentry_dsn:
+        make_sentry_logger(sentry_dsn)
+
+
+def configure_postgresql_plugin():
+
+    if "POSTGRES_DSN" in os.environ:
+        dsn = os.environ["POSTGRES_DSN"]
+    elif PSH_CONFIG.is_valid_platform():
+        dsn = PSH_CONFIG.formatted_credentials("database", "postgresql_dsn")
+    else:
+        dsn = None
+
+    if dsn:
+        return PgPlugin(
+            version=VERSION,
+            sql_migration_directory=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "../sql"
+            ),
+            dsn=dsn,
+        )
+    else:
+        raise RuntimeError(
+            "No postgresql configuration available. Use POSTGRES_DSN environment variable"
+        )
+
+
+if __name__ == "__main__":
+
+    setup_logging()
+
+    if len(sys.argv) > 1 and sys.argv[1] == "migrate":
+        postgres = configure_postgresql_plugin()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(postgres.migrate())
+        sys.exit(0)
 
     bot = SirBot()
 
@@ -64,9 +105,6 @@ if __name__ == "__main__":
     giphy = GiphyPlugin()
     bot.load_plugin(giphy)
 
-    deploy = DeployPlugin()
-    bot.load_plugin(deploy)
-
     stocks = StocksPlugin()
     bot.load_plugin(stocks)
 
@@ -78,24 +116,7 @@ if __name__ == "__main__":
     endpoints.readthedocs.register(readthedocs)
     bot.load_plugin(readthedocs)
 
-    if "POSTGRES_DSN" in os.environ:
-        dsn = os.environ["POSTGRES_DSN"]
-    elif "PLATFORM_PROJECT" in os.environ:
-        import platformshconfig
-
-        config = platformshconfig.Config()
-        dsn = config.formatted_credentials("database", "postgresql_dsn")
-    else:
-        dsn = None
-
-    if dsn:
-        postgres = PgPlugin(
-            version=VERSION,
-            sql_migration_directory=os.path.join(
-                os.path.dirname(os.path.realpath(__file__)), "../sql"
-            ),
-            dsn=dsn,
-        )
-        bot.load_plugin(postgres)
+    postgres = configure_postgresql_plugin()
+    bot.load_plugin(postgres)
 
     bot.start(host=HOST, port=PORT, print=False)
--- sirbot-pyslackers/plugins/__init__.py
+++ sirbot-pyslackers/plugins/__init__.py
@@ -1,4 +1,3 @@
 from .pypi import PypiPlugin  # noQa F401
 from .giphy import GiphyPlugin  # noQa F401
-from .deploy import DeployPlugin  # noQa F401
 from .stocks import StocksPlugin  # noQa F401
